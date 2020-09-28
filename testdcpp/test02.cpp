ff77bba0ae75450ea9c543364fae926f11a73f48
Lemmon <897545141@qq.com>
2020-09-28 19:22:35 +0800

    first try for git

---
+++ testdcpp/test02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../testh/testh.h"
+using namespace std;
+
+
+int main()
+{
+    int n=9;
+    Circle testCircle;
+    testCircle.classtest(n);
+    cout << "This is a new test file!" << endl;
+    // system("pause");
+    // getchar();
+    return 0;
+}
---
+++ testh/testh.h
@@ -0,0 +1,25 @@
+#ifndef testh
+#define testh
+#include <iostream>
+using namespace std;
+
+int show(int k)
+{
+    cout << "This is a multifolder h file test!" << endl;
+    return 0;
+}
+
+class Circle
+{
+
+public:
+    void classtest(int k)
+        {
+            show(k);
+        };
+};
+
+
+
+
+#endif
